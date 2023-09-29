10ae64bc3795666f18aaf835cd0b87963dc71217
ybondev <ybon.dev@gmail.com>
2023-09-29 21:44:21 +0800

    added dotenv

--- utils/axios.js
+++ utils/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
+const API = process.env.NEXT_API_KEY;
+
 const axiosInstance = axios.create({
   method: "GET",
   url: `https://api.themoviedb.org/3/`,
   params: { append_to_response: "videos", language: "en" },
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGUzMTNjYWU1NzkxYjdjMjc1ZDM1YWJmMGRjOWQ1YiIsInN1YiI6IjY1MDQ2MGExZTBjYTdmMDEyZWJhN2I1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KdiSZiCctQm9k7XkHEbNL-612lCJGJgDg4MPdQ_7fOc",
+    Authorization: `Bearer ${API}`,
   },
 });
 
